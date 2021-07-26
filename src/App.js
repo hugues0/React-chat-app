80d71a10ef37eecd5c04dafeb69289e8c67fabd6
hugues0 <ntwari.hugues@gmail.com>
2021-07-27 00:29:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import reactDom from 'react-dom'
+import {ChatEngine} from 'react-chat-engine'
+import './App.css'
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID="ca1083f2-c7c3-43e3-98a8-717b99003518"
+        userName="jeezy"
+        userSecret="1870"
+        renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    );
 }
 
-export default App;
+export default App
